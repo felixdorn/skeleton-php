48381918b2e377d603e7c5e93209eb92166873df
Félix Dorn <github@felixdorn.fr>
2024-03-21 12:38:07 +0100

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs =
+    {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+      flake-utils.url = "github:numtide/flake-utils";
+    };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    {
+      devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+              (php83.buildEnv {
+                  extraConfig = ''xdebug.mode=coverage'';
+
+                  extensions = { enabled, all }: enabled ++ (with all; [
+                      xdebug
+                  ]);
+              })
+              php83Packages.composer
+          ];
+      };
+    });
+}
+
